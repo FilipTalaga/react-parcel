f8af306acf2fd575b7f8c49c11477f423a611d42
Filip <talaga.filip@gmail.com>
2020-05-17 19:39:30 +0200

    Add babel presets and polyfils

---
+++ src/common/polyfils.ts
@@ -0,0 +1,5 @@
+/* Built-ins like Promise or WeakMap, static methods like Array.from or Object.assign*/
+import '@babel/polyfill';
+
+/* Fetch polyfill for both node and browser */
+import 'isomorphic-fetch';
