1018093d12b30ea63ca7ef5499d5a0dc244c4cbe
Filip <talaga.filip@gmail.com>
2020-05-17 18:23:24 +0200

    Test

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-const App = () => (
-    <div>
-        <p>This is a sample app</p>
-    </div>
-);
+interface Props {
+    active: boolean;
+}
 
+const Root = styled.div<Props>`
+    margin: ${({ active }) => (active ? '2rem' : '4rem')};
+`;
+
+const App = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+            const json = await res?.json();
+            setData(json);
+        })();
+    }, [setData]);
+
+    return (
+        <Root active={false}>
+            <p>This is a sample app</p>
+            {data.map((item: any) => item.title)}
+        </Root>
+    );
+};
 export default App;
